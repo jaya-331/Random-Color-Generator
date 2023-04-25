7b8b97974772bd0d160374f2ee016f3eb6692a66
jaya <jayarathore331@gmail.com>
2023-04-25 14:15:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const getColor = () =>{
+//hax Code
+const randomNumber = Math.floor(Math.random() * 1677215)
+const randomCode = "#"+randomNumber.toString(16);
+// console.log(randomNumber,randomCode);
+
+document.body.style.backgroundColor = randomCode;
+document.getElementById("color-code").innerText = randomCode;
+
+
+navigator.clipboard.writeText(randomCode)
+
+}
+
+
+//Event call
+document.getElementById("btn").addEventListener("click",getColor)
+
+
+//initial commit
+
+getColor()
